d179ad827a7deae6515c9af35df8c009e9c3485c
seul <ths06123@gmail.com>
2019-12-27 17:08:45 +0900

    commit

---
+++ app/src/main/java/com/example/practice/ui/main/third.java
@@ -0,0 +1,39 @@
+package com.example.practice.ui.main;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.practice.R;
+import com.example.practice.ThirdViewModel;
+
+public class third extends Fragment {
+
+    private ThirdViewModel mViewModel;
+
+    public static third newInstance() {
+        return new third();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.third_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ThirdViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
