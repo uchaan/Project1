788f1f262d6a825de40837b5d96b9d8d8785572b
seul <ths06123@gmail.com>
2019-12-27 17:13:33 +0900

    push

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "This button is nothing", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This button is nothing//", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
